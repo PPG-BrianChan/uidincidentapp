5eb4ac267e895ab0da2133a0d43863523cf94ed4
PPG-BrianChan <cchan@ppg.com>
2022-03-11 07:49:15 +0000

    Build

---
+++ app/userincidentapp/dist/Component-dbg.js
@@ -0,0 +1,9 @@
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("userincidentapp.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
